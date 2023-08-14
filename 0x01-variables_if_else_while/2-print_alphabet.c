40dc453d000eb9eae243af978fe9439168f67554
Oumelaz <abdessalamoumelaz@gmail.com>
2023-08-14 05:46:48 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
- * Return: 0
+ * Description:  get a random number and check its last digit, compare it with 5.
  */
 int main(void)
 {
