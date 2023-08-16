8255e3b9447c9e07a6e0d4c97fa07cfab4c8bb30
Oumelaz <abdessalamoumelaz@gmail.com>
2023-08-16 15:24:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ * main - Entry point
+ * Description: Write a program that prints _putchar.
+ * Return:0
+ */
+
+int main(void)
+{
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
+
